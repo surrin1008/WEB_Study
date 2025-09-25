30b02b8de42c283c9e7abab2112f8c1ecee23228
surrin1008 <20213056@office.deu.ac.kr>
2025-09-25 13:43:10 +0900

    🚧 중간저장

---
+++ class/03_javascript/연습_08_timer.js
@@ -0,0 +1,47 @@
+setTimeout(function (){
+console.log("펑")    
+},3000)
+// 1
+// VM114:2 펑
+setInterval(function () {
+console.log("1초")    
+},1000)
+// 2
+// 15VM211:2 1초
+setInterval(function(){
+    console.log(time)
+    time = time - 1
+},1000)
+
+let time = 10
+undefined
+setInterval(function(){
+    if (time>=0) {
+        console.log(time)
+        time = time - 1
+    }
+},1000)
+// 1
+// VM392:3 10
+// VM392:3 9
+// VM392:3 8
+// VM392:3 7
+// VM392:3 6
+// VM392:3 5
+// VM392:3 4
+// VM392:3 3
+// VM392:3 2
+// VM392:3 1
+// VM392:3 0
+
+let time = 180
+
+undefined
+setInterval(function(){
+    if (time >= 0) {
+        let min = Math.floor(time / 60)
+        let sec = String(time % 60).padStart(2,"0")
+        console.log(min + ":" + sec)
+        time = time - 1
+    }
+},1000)
