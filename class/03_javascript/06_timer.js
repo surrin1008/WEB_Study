d9c7fd12c9604adba93e04f94c13da660e1023a3
surrin1008 <20213056@office.deu.ac.kr>
2025-09-28 16:57:06 +0900

    🔨 내장함수 완료

--- class/03_javascript/06_timer.js
+++ class/03_javascript/06_timer.js
@@ -1,19 +1,39 @@
+let isStarted = false;
+
 const auth = () =>{
-    const token = String(Math.floor( Math.random() * 1000000)).padStart(6,"0")
-    
-    document.getElementById("target").innerText = token
-    let time = 1
-    setInterval(function(){
-        if(time >= 0)
-        {
-            let min = Math.floor(time / 60)
-            let sec = String(time % 60).padStart(2,"0")
-            document.getElementById("timer").innerText = min + ":" + sec
-            time = time - 1
-        }
-        else
-        {
-            document.getElementById("finish").disabled = true
-        }
-    }, 1000)
+
+    if(isStarted === false)
+    {
+        isStarted = true
+        document.getElementById("finish").disabled = false
+        const token = String(Math.floor( Math.random() * 1000000)).padStart(6,"0")
+        
+        document.getElementById("target").innerText = token
+        document.getElementById("target").style.color = "#" + token
+        
+        let time = 5
+        let timer
+
+        timer = setInterval(function(){
+            if(time >= 0)
+            {
+                let min = Math.floor(time / 60)
+                let sec = String(time % 60).padStart(2,"0")
+                document.getElementById("timer").innerText = min + ":" + sec
+                time = time - 1
+            }
+            else
+            {
+                document.getElementById("finish").disabled = true
+                isStarted = false
+                console.log("타이머 작동중")
+                clearInterval(timer)
+            }
+        }, 1000)
+    }
+    else
+    {
+        
+    }
 }
+
